e541fe96a7b47cea1432033f12cc5a629d15fa41
Joshua Rodriguez <ewized@gmail.com>
2015-01-01 09:46:07 -0800

    Create a default callback with no error

--- core/src/main/java/net/year4000/utilities/Callback.java
+++ core/src/main/java/net/year4000/utilities/Callback.java
@@ -3,4 +3,9 @@ package net.year4000.utilities;
 public interface Callback<T> {
     /** A simple call back interface */
     public void callback(T data, Throwable error);
+
+    /** A simple call back interface */
+    public default void callback(T data) {
+        callback(data, null);
+    }
 }
