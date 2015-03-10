cdb9a6a6d15f41bb4125ecc7f978a13fce583dbf
Joshua Rodriguez <ewized@gmail.com>
2015-03-10 09:53:07 -0700

    Update API interface to allow for async

--- core/src/main/java/net/year4000/utilities/api/API.java
+++ core/src/main/java/net/year4000/utilities/api/API.java
@@ -1,15 +1,37 @@
 package net.year4000.utilities.api;
 
-import lombok.Data;
+import com.google.gson.JsonObject;
+import net.year4000.utilities.Callback;
+import net.year4000.utilities.api.routes.Route;
+import net.year4000.utilities.api.routes.accounts.AccountRoute;
 
-import java.net.URL;
-import java.util.Map;
+public final class API {
+    public static final String BASE_URL = "https://api.year4000.net/";
 
-@Data
-public class API {
-    String name;
-    String vendor;
-    String version;
-    URL documentation;
-    Map<String, String> routes;
+    /** Get the account with the id of the user */
+    public static AccountRoute getAccount(String id) {
+        try {
+            JsonObject response = HttpFetcher.get(BASE_URL + "accounts/" + id, JsonObject.class);
+            return Route.generate(AccountRoute.class, response);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /** Get the account with the id of the user */
+    public static void getAccountAsync(String id, Callback<AccountRoute> callback) {
+        JsonObject response = null;
+        Throwable error = null;
+
+        try {
+            response = HttpFetcher.get(BASE_URL + "accounts/" + id, JsonObject.class);
+        }
+        catch (Exception e) {
+            error = e;
+        }
+        finally {
+            callback.callback(Route.generate(AccountRoute.class, response), error);
+        }
+    }
 }
