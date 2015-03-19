8d960eef65740fb040bf93fae225859831f7ad3b
Joshua Rodriguez <ewized@gmail.com>
2015-03-18 19:34:58 -0700

    Added support for reflection types

--- core/src/main/java/net/year4000/utilities/api/HttpFetcher.java
+++ core/src/main/java/net/year4000/utilities/api/HttpFetcher.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
+import java.lang.reflect.Type;
 import java.net.URL;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -79,10 +80,35 @@ public class HttpFetcher {
         });
     }
 
+    /** HTTP get method with async request */
+    public static <T> void get(String url, Type type, Callback<T> callback) {
+        SCHEDULER.run(() -> {
+            T response = null;
+            Throwable error = null;
+
+            try {
+                response = get(url, type);
+            }
+            catch (Exception e) {
+                error = e;
+            }
+            finally {
+                callback.callback(response, error);
+            }
+        });
+    }
+
     /** HTTP get method with sync request */
     public static <T> T get(String url, Class<T> clazz) throws Exception {
         try (Reader reader = request(Methods.GET, url)) {
             return GSON.fromJson(reader, clazz);
+        }
+    }
+
+    /** HTTP get method with sync request */
+    public static <T> T get(String url, Type type) throws Exception {
+        try (Reader reader = request(Methods.GET, url)) {
+            return GSON.fromJson(reader, type);
         }
     }
 
@@ -104,10 +130,35 @@ public class HttpFetcher {
         });
     }
 
+    /** HTTP post method with async request */
+    public static <T> void post(String url, JsonObject data, Type type, Callback<T> callback) {
+        SCHEDULER.run(() -> {
+            T response = null;
+            Throwable error = null;
+
+            try {
+                response = post(url, data, type);
+            }
+            catch (Exception e) {
+                error = e;
+            }
+            finally {
+                callback.callback(response, error);
+            }
+        });
+    }
+
     /** HTTP post method with sync request */
     public static <T> T post(String url, JsonObject data, Class<T> clazz) throws Exception {
         try (Reader reader = request(Methods.POST, data, url)) {
             return GSON.fromJson(reader, clazz);
+        }
+    }
+
+    /** HTTP post method with sync request */
+    public static <T> T post(String url, JsonObject data, Type type) throws Exception {
+        try (Reader reader = request(Methods.POST, data, url)) {
+            return GSON.fromJson(reader, type);
         }
     }
 
@@ -129,10 +180,35 @@ public class HttpFetcher {
         });
     }
 
+    /** HTTP put method with async request */
+    public static <T> void put(String url, JsonObject data, Type type, Callback<T> callback) {
+        SCHEDULER.run(() -> {
+            T response = null;
+            Throwable error = null;
+
+            try {
+                response = post(url, data, type);
+            }
+            catch (Exception e) {
+                error = e;
+            }
+            finally {
+                callback.callback(response, error);
+            }
+        });
+    }
+
     /** HTTP put method with sync request */
     public static <T> T put(String url, JsonObject data, Class<T> clazz) throws Exception {
         try (Reader reader = request(Methods.PUT, data, url)) {
             return GSON.fromJson(reader, clazz);
+        }
+    }
+
+    /** HTTP put method with sync request */
+    public static <T> T put(String url, JsonObject data, Type type) throws Exception {
+        try (Reader reader = request(Methods.PUT, data, url)) {
+            return GSON.fromJson(reader, type);
         }
     }
 
@@ -154,10 +230,35 @@ public class HttpFetcher {
         });
     }
 
+    /** HTTP delete method with async request */
+    public static <T> void delete(String url, JsonObject data, Type type, Callback<T> callback) {
+        SCHEDULER.run(() -> {
+            T response = null;
+            Throwable error = null;
+
+            try {
+                response = post(url, data, type);
+            }
+            catch (Exception e) {
+                error = e;
+            }
+            finally {
+                callback.callback(response, error);
+            }
+        });
+    }
+
     /** HTTP delete method with sync request */
     public static <T> T delete(String url, JsonObject data, Class<T> clazz) throws Exception {
         try (Reader reader = request(Methods.DELETE, data, url)) {
             return GSON.fromJson(reader, clazz);
+        }
+    }
+
+    /** HTTP delete method with sync request */
+    public static <T> T delete(String url, JsonObject data, Type type) throws Exception {
+        try (Reader reader = request(Methods.DELETE, data, url)) {
+            return GSON.fromJson(reader, type);
         }
     }
 }
