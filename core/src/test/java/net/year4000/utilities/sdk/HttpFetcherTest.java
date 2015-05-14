34f3dcf272885101a150dd0f69bf92e538ae3815
Joshua Rodriguez <ewized@gmail.com>
2015-05-14 10:03:21 -0700

    Modify the HttpFetcherTest to make the test pass

--- core/src/test/java/net/year4000/utilities/sdk/HttpFetcherTest.java
+++ core/src/test/java/net/year4000/utilities/sdk/HttpFetcherTest.java
@@ -3,6 +3,7 @@ package net.year4000.utilities.sdk;
 import com.google.gson.JsonObject;
 import junit.framework.Assert;
 import lombok.extern.java.Log;
+import net.year4000.utilities.URLBuilder;
 import net.year4000.utilities.sdk.HttpFetcher;
 import org.junit.Test;
 
@@ -10,12 +11,32 @@ import org.junit.Test;
 public class HttpFetcherTest {
     private static final String URL = "https://api.year4000.net";
     private static final String OFFLINE = "502 Bad Gateway";
+    private enum Methods {GET, POST, PUT, DELETE}
 
-    @Test
-    public void getTest() {
+    private void test(Methods method) {
         try {
-            JsonObject response = HttpFetcher.get(URL, JsonObject.class);
-            Assert.assertEquals(response.get("name").getAsString(), "Year4000-API");
+            String code = String.valueOf(System.currentTimeMillis());
+            URLBuilder.builder(URL).addPath("test").addQuery("code", code);
+            JsonObject response;
+
+            switch (method) {
+                case GET:
+                    response = HttpFetcher.get(URL, JsonObject.class);
+                    break;
+                case POST:
+                    response = HttpFetcher.post(URL, null, JsonObject.class);
+                    break;
+                case PUT:
+                    response = HttpFetcher.put(URL, null, JsonObject.class);
+                    break;
+                case DELETE:
+                    response = HttpFetcher.delete(URL, null, JsonObject.class);
+                    break;
+                default:
+                    throw new EnumConstantNotPresentException(Methods.class, method.toString());
+            }
+
+            Assert.assertEquals(code, response.get("code").getAsString());
         }
         catch (Exception e) {
             if (!e.getMessage().equals(OFFLINE)) {
@@ -25,38 +46,22 @@ public class HttpFetcherTest {
     }
 
     @Test
+    public void getTest() {
+        test(Methods.GET);
+    }
+
+    @Test
     public void postTest() {
-        try {
-           HttpFetcher.post(URL, null, JsonObject.class);
-        }
-        catch (Exception e) {
-            if (!e.getMessage().equals(OFFLINE)) {
-                Assert.assertEquals(e.getMessage(), "405 Method Not Allowed");
-            }
-        }
+        test(Methods.POST);
     }
 
     @Test
     public void putTest() {
-        try {
-            HttpFetcher.put(URL, null, JsonObject.class);
-        }
-        catch (Exception e) {
-            if (!e.getMessage().equals(OFFLINE)) {
-                Assert.assertEquals(e.getMessage(), "405 Method Not Allowed");
-            }
-        }
+        test(Methods.PUT);
     }
 
     @Test
     public void deleteTest() {
-        try {
-            HttpFetcher.delete(URL, null, JsonObject.class);
-        }
-        catch (Exception e) {
-            if (!e.getMessage().equals(OFFLINE)) {
-                Assert.assertEquals(e.getMessage(), "405 Method Not Allowed");
-            }
-        }
+        test(Methods.DELETE);
     }
 }
