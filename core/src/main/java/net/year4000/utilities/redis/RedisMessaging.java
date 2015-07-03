c0e938d51bc11db87762a62e0d492ce8e7e37a0a
Joshua Rodriguez <ewized@gmail.com>
2015-07-03 09:11:11 -0700

    Add RedisMessaging

---
+++ core/src/main/java/net/year4000/utilities/redis/RedisMessaging.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2015 Year4000.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.year4000.utilities.redis;
+
+import com.google.common.collect.Maps;
+import lombok.AllArgsConstructor;
+import net.year4000.utilities.scheduler.SchedulerManager;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPubSub;
+
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.Consumer;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+@AllArgsConstructor
+public class RedisMessaging {
+    private static final String CHANNELS = "*";
+    private SchedulerManager scheduler;
+    private JedisPool jedisPool;
+    private final ConcurrentMap<String, Consumer<String>> listeners = Maps.newConcurrentMap();
+
+    /** Init the Redis messaging thread and return self */
+    public RedisMessaging init() {
+        scheduler.run(() -> {
+            try (Jedis jedis = jedisPool.getResource()) {
+                jedis.psubscribe(new PubSub(), CHANNELS);
+            }
+        });
+
+        return this;
+    }
+
+    /** Unregister a listener from the Redis channel */
+    public void unsubscribe(String channel) {
+        checkNotNull(channel);
+        listeners.remove(channel);
+    }
+
+    /** Register a listener on the Redis channel */
+    public void subscribe(String channel, Consumer<String> data) {
+        checkNotNull(channel);
+        checkNotNull(data);
+        listeners.put(channel, data);
+    }
+
+    /** Publish a message on the channel */
+    public void publish(String channel, String message) {
+        checkNotNull(channel);
+        checkNotNull(message);
+
+        try (Jedis jedis = jedisPool.getResource()) {
+            jedis.publish(channel, message);
+        }
+    }
+
+    /** Handle the message from Redis */
+    public class PubSub extends JedisPubSub {
+        @Override
+        public void onPMessage(String pattern, String channel, String message) {
+            if (listeners.containsKey(channel)) {
+                listeners.get(channel).accept(message);
+            }
+        }
+    }
+}
