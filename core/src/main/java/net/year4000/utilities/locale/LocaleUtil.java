d75f8b78a0e7999f99aa187cdf097f4c6c4fe015
Joshua Rodriguez <ewized@gmail.com>
2015-08-26 16:04:38 -0700

    Improve locales with Object and Enum support closes Year4000/Issues#36

--- core/src/main/java/net/year4000/utilities/locale/LocaleUtil.java
+++ core/src/main/java/net/year4000/utilities/locale/LocaleUtil.java
@@ -21,4 +21,15 @@ package net.year4000.utilities.locale;
 public interface LocaleUtil {
     /** Translate to the specific locale with formatting */
     String get(String key, Object... args);
+
+    /** Translate to the specific locale with formatting */
+    default String get(Enum<?> key, Object... args) {
+        String formatted = key.name().toLowerCase()
+            .replace("__", "-") // Double underscore represent a dash
+            .replace("_", ".") // Underscore represents a dot
+            .replace("-", "_") // Dash represents an underscore
+            ;
+
+        return get(formatted, args);
+    }
 }
