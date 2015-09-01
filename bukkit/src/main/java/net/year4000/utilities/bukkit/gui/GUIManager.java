7eac3d70012724f59f8a5987042872caa83e0738
Joshua Rodriguez <ewized@gmail.com>
2015-09-01 11:09:10 -0700

    Check if inventory is null

--- bukkit/src/main/java/net/year4000/utilities/bukkit/gui/GUIManager.java
+++ bukkit/src/main/java/net/year4000/utilities/bukkit/gui/GUIManager.java
@@ -67,7 +67,7 @@ public class GUIManager implements Listener {
         Player player;
 
         // If a player click inventory get their locale else return
-        if (event.getWhoClicked() instanceof Player) {
+        if (inventory != null && event.getWhoClicked() instanceof Player) {
             player = (Player) event.getWhoClicked();
             locale = this.locale.apply(player);
         }
