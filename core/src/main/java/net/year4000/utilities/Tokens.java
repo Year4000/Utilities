710f38a24aab650cdde88e7f3e5653b053d8b30a
Joshua Rodriguez <ewized@gmail.com>
2016-02-11 09:59:18 -0800

    Gradle things

---
+++ core/src/main/java/net/year4000/utilities/Tokens.java
@@ -0,0 +1,12 @@
+package net.year4000.utilities;
+
+/**
+ * Tokens generated from gradle.
+ */
+public class Tokens {
+    /** The version of this gradle project */
+    public static final String VERSION = "${version}";
+
+    /** The git hash of this gradle project */
+    public static final String GIT_HASH = "${git_hash}";
+}
