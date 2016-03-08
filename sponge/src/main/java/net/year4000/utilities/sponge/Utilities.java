461d0ea7d9918250a5966d5a1591d17f6a0fc896
Joshua Rodriguez <ewized@gmail.com>
2016-03-08 11:28:39 -0800

    Update to SpongeAPI 4.1-SNAPSHOT

--- sponge/src/main/java/net/year4000/utilities/sponge/Utilities.java
+++ sponge/src/main/java/net/year4000/utilities/sponge/Utilities.java
@@ -4,13 +4,21 @@
 
 package net.year4000.utilities.sponge;
 
+import net.year4000.utilities.Tokens;
 import net.year4000.utilities.sponge.command.PluginCommand;
 import net.year4000.utilities.sponge.command.SystemCommand;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.plugin.Plugin;
 
-@Plugin(id = "net.year4000.utilities", name = "Utilities")
+@Plugin(
+    id = "net.year4000.utilities",
+    name = "Utilities",
+    version = Tokens.FULL_VERSION,
+    description = "A collection of API's to make coding faster and simpler.",
+    url = "https://www.year4000.net/",
+    authors = {"ewized"}
+)
 public final class Utilities extends AbstractSpongePlugin {
 
     /** Get the instance of Utilities */
