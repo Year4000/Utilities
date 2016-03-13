cb5b5dc999f63ec5a76ee97aeadc112b1eb7b747
Joshua Rodriguez <ewized@gmail.com>
2016-03-12 20:00:40 -0800

    Fix redis tests fails when no redis is found

--- core/redis/src/test/java/net/year4000/utilities/redis/RedisCallbackTest.java
+++ core/redis/src/test/java/net/year4000/utilities/redis/RedisCallbackTest.java
@@ -4,11 +4,9 @@
 
 package net.year4000.utilities.redis;
 
-import org.junit.Assert;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.exceptions.JedisConnectionException;
 
 public class RedisCallbackTest {
     private final String listen = "year4000.utilities.callback";
@@ -25,19 +23,24 @@ public class RedisCallbackTest {
         thread.start();
     }
 
-    @After
-    public void after() {
-        thread.interrupt();
-        messaging.close();
-        pool.close();
-    }
-
     @Test
     public void test() throws Exception {
         try (RedisCallback callback = new RedisCallback(messaging, listen)) {
             Thread.sleep(250);
             String response = callback.send(listen, string);
             Assert.assertEquals(string, response);
+        } catch (JedisConnectionException error) {
+            thread.interrupt();
+            Assume.assumeTrue(false);
         }
+    }
+
+    @After
+    public void after() {
+        if (!thread.isAlive()) return;
+
+        thread.interrupt();
+        messaging.close();
+        pool.close();
     }
 }
