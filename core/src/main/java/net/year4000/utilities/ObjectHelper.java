6abf20999901dcf84fa6e94b04900b10f3f7e2fa
Joshua Rodriguez <ewized@gmail.com>
2016-04-07 10:00:06 -0700

    Add some more useful object helper utilities

--- core/src/main/java/net/year4000/utilities/ObjectHelper.java
+++ core/src/main/java/net/year4000/utilities/ObjectHelper.java
@@ -15,6 +15,172 @@ public final class ObjectHelper {
         UtilityConstructError.raise();
     }
 
+    /** Check that the value is in the states */
+    @SafeVarargs
+    public static <T extends Enum<?>> T checkState(T value, T... states) {
+        isLarger(states.length, 1);
+        for (T state : states) {
+            if (state == value) {
+                return value;
+            }
+        }
+        throw new IllegalStateException(String.format("Value %s is not in %s", value, Joiner.on(", ").join(states)));
+    }
+
+    private enum Ranges {RANGE, LARGER, SMALLER}
+
+    /** Spits out the right message for range checks */
+    private static String rangeMessage(Ranges range, Number value, Number state) {
+        nonNull(range, "range");
+        String first = String.valueOf(nonNull(value, "value"));
+        String second = String.valueOf(nonNull(state, "state"));
+        switch (range) {
+            case RANGE:
+                return String.format("Min %s must be smaller than Max %s", first, second);
+            case LARGER:
+                return String.format("Value %s must be larger than Min %s", first, second);
+            case SMALLER:
+                return String.format("Value %s must be smaller than Max %s", first, second);
+        }
+        return "null";
+    }
+
+    /** Checks if the value is smaller than the max */
+    public static byte isSmaller(byte value, byte max) {
+        return inRange(value, Byte.MIN_VALUE, max);
+    }
+
+    /** Checks if the value is larger than the min */
+    public static byte isLarger(byte value, byte min) {
+        return inRange(value, min, Byte.MAX_VALUE);
+    }
+
+    /** Checks if the value is in the range */
+    public static byte inRange(byte value, byte min, byte max) {
+        if (min > max) {
+            throw new IllegalArgumentException(rangeMessage(Ranges.RANGE, min, max));
+        }
+        if (value < min) {
+            throw new IllegalArgumentException(rangeMessage(Ranges.LARGER, value, min));
+        }
+        if (value > max) {
+            throw new IllegalArgumentException(rangeMessage(Ranges.SMALLER, value, max));
+        }
+        return value;
+    }
+
+    /** Checks if the value is smaller than the max */
+    public static short isSmaller(short value, short max) {
+        return inRange(value, Short.MIN_VALUE, max);
+    }
+
+    /** Checks if the value is larger than the min */
+    public static short isLarger(short value, short min) {
+        return inRange(value, min, Short.MAX_VALUE);
+    }
+
+    /** Checks if the value is in the range */
+    public static short inRange(short value, short min, short max) {
+        if (min > max) {
+            throw new IllegalArgumentException(rangeMessage(Ranges.RANGE, min, max));
+        }
+        if (value < min) {
+            throw new IllegalArgumentException(rangeMessage(Ranges.LARGER, value, min));
+        }
+        if (value > max) {
+            throw new IllegalArgumentException(rangeMessage(Ranges.SMALLER, value, max));
+        }
+        return value;
+    }
+
+    /** Checks if the value is smaller than the max */
+    public static float isSmaller(float value, float max) {
+        return inRange(value, Float.NEGATIVE_INFINITY, max);
+    }
+
+    /** Checks if the value is larger than the min */
+    public static float isLarger(float value, float min) {
+        return inRange(value, min, Float.POSITIVE_INFINITY);
+    }
+
+    /** Checks if the value is in the range */
+    public static float inRange(float value, float min, float max) {
+        if (min > max) {
+            throw new IllegalArgumentException(rangeMessage(Ranges.RANGE, min, max));
+        }
+        if (value < min) {
+            throw new IllegalArgumentException(rangeMessage(Ranges.LARGER, value, min));
+        }
+        if (value > max) {
+            throw new IllegalArgumentException(rangeMessage(Ranges.SMALLER, value, max));
+        }
+        return value;
+    }
+
+    /** Checks if the value is smaller than the max */
+    public static double isSmaller(double value, double max) {
+        return inRange(value, Double.NEGATIVE_INFINITY, max);
+    }
+
+    /** Checks if the value is larger than the min */
+    public static double isLarger(double value, double min) {
+        return inRange(value, min, Double.POSITIVE_INFINITY);
+    }
+
+    /** Checks if the value is in the range */
+    public static double inRange(double value, double min, double max) {
+        if (min > max) {
+            throw new IllegalArgumentException(rangeMessage(Ranges.RANGE, min, max));
+        }
+        if (value < min) {
+            throw new IllegalArgumentException(rangeMessage(Ranges.LARGER, value, min));
+        }
+        if (value > max) {
+            throw new IllegalArgumentException(rangeMessage(Ranges.SMALLER, value, max));
+        }
+        return value;
+    }
+
+    /** Checks if the value is smaller than the max */
+    public static int isSmaller(int value, int max) {
+        return inRange(value, Integer.MIN_VALUE, max);
+    }
+
+    /** Checks if the value is larger than the min */
+    public static int isLarger(int value, int min) {
+        return inRange(value, min, Integer.MAX_VALUE);
+    }
+
+    /** Checks if the value is in the range */
+    public static int inRange(int value, int min, int max) {
+        if (min > max) {
+            throw new IllegalArgumentException(rangeMessage(Ranges.RANGE, min, max));
+        }
+        if (value < min) {
+            throw new IllegalArgumentException(rangeMessage(Ranges.LARGER, value, min));
+        }
+        if (value > max) {
+            throw new IllegalArgumentException(rangeMessage(Ranges.SMALLER, value, max));
+        }
+        return value;
+    }
+
+    /** Require the string to be non null or empty, or throw NullPointerException */
+    public static String nonNullOrEmpty(String value, Object message) {
+        if (value == null || value.isEmpty()) {
+            throw new NullPointerException(String.valueOf(message));
+        }
+        return value;
+    }
+
+    /** Require the object is non null, or throw NullPointerException */
+    public static <T> T nonNull(T value, Object message) {
+        if (value == null) {
+            throw new NullPointerException(String.valueOf(message));
+        }
+        return value;
+    }
+
     /** Grab the field state value and name, used for toString */
     private static Optional<String> fieldState(Field field, Object instance) {
         Optional<Object> inst = fieldInstance(field, instance);
--- core/src/test/java/net/year4000/utilities/ObjectHelperTest.java
+++ core/src/test/java/net/year4000/utilities/ObjectHelperTest.java
@@ -2,6 +2,8 @@ package net.year4000.utilities;
 
 import org.junit.Test;
 
+import static net.year4000.utilities.ObjectHelperTest.MyEnum.*;
+
 public class ObjectHelperTest {
     private static class MyObject {
         private String bar = "bar";
@@ -28,5 +30,48 @@ public class ObjectHelperTest {
         System.err.println(new MyObject().toString());
         System.err.println(new MyObject().hashCode());
         System.err.println(new MyObject().equals(new MyObject()));
+    }
+
+    enum MyEnum {ONE, TWO, THREE, FOUR}
+
+    @Test
+    public void rangeTests() {
+        ObjectHelper.checkState(ONE, ONE, TWO, THREE, FOUR);
+
+        // Bytes
+        ObjectHelper.isLarger((byte) 0x1, (byte) 0x0);
+        ObjectHelper.isSmaller((byte) 0x0, (byte) 0x1);
+        ObjectHelper.inRange((byte) 0x1, (byte) 0x0, (byte) 0x2);
+
+        // Shorts
+        ObjectHelper.isLarger((short) 1, (short) 0);
+        ObjectHelper.isSmaller((short) 0, (short) 1);
+        ObjectHelper.inRange((short) 1, 0, (short) 2);
+
+        // Integers
+        ObjectHelper.isLarger(1, 0);
+        ObjectHelper.isSmaller(0, 1);
+        ObjectHelper.inRange(1, 0, 2);
+
+        // Float
+        ObjectHelper.isLarger((float) 0.1, (float) 0.0);
+        ObjectHelper.isSmaller((float) 0.0, (float) 0.1);
+        ObjectHelper.inRange((float) 0.1, (float) 0.0, (float) 0.2);
+
+        // Longs
+        ObjectHelper.isLarger((long) 0.1, (long) 0.0);
+        ObjectHelper.isSmaller((long) 0.0, (long) 0.1);
+        ObjectHelper.inRange((long) 0.1, (long) 0.0, (long) 0.2);
+
+        // Doubles
+        ObjectHelper.isLarger(0.1, 0.0);
+        ObjectHelper.isSmaller(0.0, 0.1);
+        ObjectHelper.inRange(0.1, 0.0, 0.2);
+    }
+
+    @Test
+    public void nullChecks() {
+        ObjectHelper.nonNull(new Object(), "object");
+        ObjectHelper.nonNullOrEmpty("String", "string");
     }
 }
