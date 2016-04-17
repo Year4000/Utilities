5874cb7a94554b1085bf4062befaaabe5e88bcb1
Joshua Rodriguez <ewized@gmail.com>
2016-04-16 21:00:23 -0700

    Addmore utils methods

--- core/src/main/java/net/year4000/utilities/Utils.java
+++ core/src/main/java/net/year4000/utilities/Utils.java
@@ -5,14 +5,78 @@ import net.year4000.utilities.utils.UtilityConstructError;
 import net.year4000.utilities.value.Value;
 
 import java.lang.reflect.Field;
+import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /** Contains basic util methods that do not belong in a set category */
 public final class Utils {
     private Utils() {
         UtilityConstructError.raise();
+    }
+
+    /** Does the string match what a uuid should look like */
+    public static boolean isUUID(String uuid) {
+        return uuid != null && !uuid.isEmpty() && uuid.split("-").length == 5 && uuid.length() <= 36;
+    }
+
+    /** Convert a string uuid to UUID object*/
+    public static UUID toUUID(String uuid) {
+        Conditions.nonNullOrEmpty(uuid, "uuid");
+        if (isUUID(uuid)) {
+            return UUID.fromString(uuid);
+        } else {
+            return UUID.fromString(uuid.replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5"));
+        }
+    }
+
+    /** Validate the ip */
+    public static boolean isIpAddress(String ip) {
+        if (ip == null || ip.isEmpty()) {
+            return false;
+        }
+        // IP6
+        if (ip.indexOf(':') > 0 && ip.split(":").length == 8) {
+            return true;
+        }
+        // IP4
+        String[] parts = ip.split("\\.");
+        if (parts.length != 4) {
+            return false;
+        }
+        for (String part : parts) {
+            Value<Integer> ipPart = Value.parseInteger(part);
+            if (ipPart.isEmpty()) {
+                return false;
+            }
+            if (ipPart.get() < 0 && ipPart.get() > 256) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /** Get the server name */
+    public static String hostName() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        }
+        catch (Exception e) {
+            return "unknown";
+        }
+    }
+
+    /** Sleep the current thread */
+    public static void quickSleep() {
+        try {
+            Thread.sleep(Byte.MAX_VALUE);
+        } catch (InterruptedException error) {
+            throw new RuntimeException(error);
+        }
     }
 
     /** Grab the field state value and name, used for toString */
--- core/src/test/java/net/year4000/utilities/UtilsTest.java
+++ core/src/test/java/net/year4000/utilities/UtilsTest.java
@@ -1,6 +1,10 @@
 package net.year4000.utilities;
 
+import com.google.common.base.Stopwatch;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.UUID;
 
 public class UtilsTest {
     private static class MyObject {
@@ -26,8 +30,30 @@ public class UtilsTest {
 
     @Test
     public void test() {
-        System.err.println(new MyObject().toString());
-        System.err.println(new MyObject().hashCode());
-        System.err.println(new MyObject().equals(new MyObject()));
+        System.out.println(new MyObject().toString());
+        System.out.println(new MyObject().hashCode());
+        System.out.println(new MyObject().equals(new MyObject()));
+        System.out.println(Utils.hostName());
+    }
+
+    @Test
+    public void sleepTest() {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        Utils.quickSleep();
+        System.out.println("Utils.quickSleep() lasted for: " + stopwatch.stop());
+    }
+
+    @Test
+    public void checkersTest() {
+        Assert.assertTrue(Utils.isIpAddress("127.0.0.1"));
+        Assert.assertFalse(Utils.isIpAddress("a.b.c.d"));
+    }
+
+    @Test
+    public void uuidTest() {
+        final UUID uuid = UUID.randomUUID();
+        final String stringUuid = uuid.toString();
+        Assert.assertTrue(Utils.isUUID(stringUuid));
+        Assert.assertEquals(uuid, Utils.toUUID(stringUuid));
     }
 }
