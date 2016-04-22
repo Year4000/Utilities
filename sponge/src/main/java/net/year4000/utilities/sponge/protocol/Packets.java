2ce9b96f92ad7c37d9923c0423b337fe371496b0
Joshua Rodriguez <ewized@gmail.com>
2016-04-22 16:41:46 -0700

    Packets can pick to use utilities for the plugin type

--- sponge/src/main/java/net/year4000/utilities/sponge/protocol/Packets.java
+++ sponge/src/main/java/net/year4000/utilities/sponge/protocol/Packets.java
@@ -1,11 +1,16 @@
 package net.year4000.utilities.sponge.protocol;
 
+import net.year4000.utilities.sponge.Utilities;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 
-
 public interface Packets {
-    /** Get the default PacketManager for this interface */
+    /** Get the default PacketManager for this interface using utilities as the plugin */
+    static Packets manager() {
+        return manager(Utilities.get());
+    }
+
+    /** Get the default PacketManager for this interface using the plugin you supplied */
     static Packets manager(Object plugin) {
         return new PacketManager(plugin);
     }
