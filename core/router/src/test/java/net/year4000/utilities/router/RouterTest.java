1f48ddeaab533cd578b23612b6fbfc6150bb30db
Joshua Rodriguez <ewized@gmail.com>
2016-04-27 16:44:05 -0700

    Add unit test for router

---
+++ core/router/src/test/java/net/year4000/utilities/router/RouterTest.java
@@ -0,0 +1,13 @@
+package net.year4000.utilities.router;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RouterTest {
+    @Test
+    public void routerBuilderTest() {
+        Router router = Router.builder().path("test", "GET", String.class, (request, response, args) -> "test").build();
+        Assert.assertTrue(router.findPath("test").isPresent());
+        Assert.assertFalse(router.findPath("text").isPresent());
+    }
+}
