595f8277283c4790454d24ba866370588f17febb
Joshua Rodriguez <ewized@gmail.com>
2016-05-03 23:03:58 -0700

    It should be the signature not the value

--- sponge/src/main/java/net/year4000/utilities/sponge/protocol/proxy/ProxyNetworkManager.java
+++ sponge/src/main/java/net/year4000/utilities/sponge/protocol/proxy/ProxyNetworkManager.java
@@ -7,6 +7,6 @@ import net.year4000.utilities.reflection.annotations.Proxied;
 @Proxied("net.minecraft.network.NetworkManager")
 public interface ProxyNetworkManager {
     /** The netty channel that is attached to the network manager */
-    @Getter("Lio/netty/channel/Channel;")
+    @Getter(signature = "Lio/netty/channel/Channel;")
     Channel channel();
 }
