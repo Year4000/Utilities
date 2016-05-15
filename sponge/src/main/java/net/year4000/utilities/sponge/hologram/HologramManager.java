e367429f608b3cae7c7617794784eee53b24b065
Joshua Rodriguez <ewized@gmail.com>
2016-05-14 20:44:23 -0700

    Have holograms work with static holograms

--- sponge/src/main/java/net/year4000/utilities/sponge/hologram/HologramManager.java
+++ sponge/src/main/java/net/year4000/utilities/sponge/hologram/HologramManager.java
@@ -1,17 +1,22 @@
 package net.year4000.utilities.sponge.hologram;
 
-import com.flowpowered.math.vector.Vector3d;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
 import net.year4000.utilities.Conditions;
+import net.year4000.utilities.ErrorReporter;
 import net.year4000.utilities.sponge.protocol.Packets;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
-import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
+
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
 
 /**
  * The Hologram Manager that will handle all of the holograms, it uses a FrameBuffer to
@@ -19,6 +24,7 @@ import java.util.Map;
  */
 public class HologramManager implements Holograms {
     private static final Map<Class<?>, HologramManager> managers = Maps.newConcurrentMap();
+    final Multimap<Hologram, Player> animationViewers = MultimapBuilder.ListMultimapBuilder.treeKeys().hashSetValues().build();
     final Object plugin;
     final Packets packets;
 
@@ -34,23 +40,24 @@ public class HologramManager implements Holograms {
         return managers.get(clazz);
     }
 
-    @Override public Hologram add(Location<World> location, Text... text) {
-        return null;
+    @Override
+    public Hologram add(Collection<Player> players, Location<World> location, Text... texts) {
+        Hologram hologram = new Hologram(this, location, FrameBuffer.builder().add(texts).build());
+        send(players, hologram);
+        return hologram;
     }
 
-    @Override public Hologram add(Collection<Player> players, Vector3d vector, Text... text) {
-        return null;
-    }
-
-    @Override public Hologram add(Location<World> location, BufferedImage image) {
-        return null;
-    }
-
-    @Override public Hologram add(Collection<Player> players, Vector3d vector, BufferedImage image) {
-        return null;
-    }
-
-    @Override public void remove(Player player, Hologram hologram) {
-
+    @Override
+    public Hologram add(Collection<Player> players, Location<World> location, ImageInputStream image) {
+        try {
+            Hologram hologram = new Hologram(this, location, FrameBuffer.builder().add(ImageIO.read(image)).build());
+            send(players, hologram);
+            return hologram;
+        } catch (IOException exception) {
+            throw ErrorReporter.builder(exception)
+                .add("Player(s): ", players)
+                .add("Location: ", location)
+                .buildAndReport(System.err);
+        }
     }
 }
--- sponge/src/main/java/net/year4000/utilities/sponge/hologram/Holograms.java
+++ sponge/src/main/java/net/year4000/utilities/sponge/hologram/Holograms.java
@@ -1,8 +1,7 @@
 package net.year4000.utilities.sponge.hologram;
 
-import com.flowpowered.math.vector.Vector3d;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import net.year4000.utilities.sponge.Utilities;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
@@ -10,8 +9,9 @@ import org.spongepowered.api.text.Text;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
-import java.awt.image.BufferedImage;
 import java.util.Collection;
+
+import javax.imageio.stream.ImageInputStream;
 
 /** Create holograms */
 public interface Holograms {
@@ -26,37 +26,73 @@ public interface Holograms {
     }
 
     /** Add a text hologram at the specific location */
-    Hologram add(Location<World> location, Text... text);
+    default Hologram add(Location<World> location, Text... texts) {
+        return add(Sponge.getServer().getOnlinePlayers(), location, texts);
+    }
 
     /** Add a text hologram for the player at the location */
-    default Hologram add(Player player, Vector3d vector, Text... text) {
-        return add(Lists.newArrayList(player), vector, text);
+    default Hologram add(Player player, Location<World> location, Text... texts) {
+        return add(ImmutableList.of(player), location, texts);
     }
 
     /** Add a text hologram for the collection of players at the location */
-    Hologram add(Collection<Player> players, Vector3d vector, Text... text);
+    Hologram add(Collection<Player> players, Location<World> location, Text... texts);
 
     /** Add a hologram from the image at the specific location */
-    Hologram add(Location<World> location, BufferedImage image);
+    default Hologram add(Location<World> location, ImageInputStream image) {
+        return add(Sponge.getServer().getOnlinePlayers(), location, image);
+    }
 
     /** Add a image hologram for the player at the location */
-    default Hologram add(Player player, Vector3d vector, BufferedImage image) {
-        return add(Lists.newArrayList(player), vector, image);
+    default Hologram add(Player player, Location<World> location, ImageInputStream image) {
+        return add(ImmutableList.of(player), location, image);
     }
 
     /** Add a image hologram for the set of players at the location */
-    default Hologram add(Vector3d vector, BufferedImage image, Player... players) {
-        return add(Sets.newHashSet(players), vector, image);
+    default Hologram add(Location<World> location, ImageInputStream image, Player... players) {
+        return add(ImmutableSet.copyOf(players), location, image);
     }
 
     /** Add a image hologram for the collection of players at the location */
-    Hologram add(Collection<Player> players, Vector3d vector, BufferedImage image);
+    Hologram add(Collection<Player> players, Location<World> location, ImageInputStream image);
 
     /** Remove a hologram from the players */
-    void remove(Player player, Hologram hologram);
+    default void remove(Player player, Hologram hologram) {
+        remove(ImmutableList.of(player), hologram);
+    }
 
     /** Remove a hologram from all players */
     default void remove(Hologram hologram) {
-        Sponge.getServer().getOnlinePlayers().forEach(player -> remove(player, hologram));
+        remove(Sponge.getServer().getOnlinePlayers(), hologram);
+    }
+
+    /** Remove a hologram from the set of players */
+    default void remove(Hologram hologram, Player... players) {
+        remove(ImmutableSet.copyOf(players), hologram);
+    }
+
+    /** Remove a hologram from the collection of players */
+    default void remove(Collection<Player> players, Hologram hologram) {
+        players.forEach(hologram::destroy);
+    }
+
+    /** Send a hologram to the specific player */
+    default void send(Player player, Hologram hologram) {
+        hologram.send(player);
+    }
+
+    /** Send a hologram to the set of players */
+    default void send(Hologram hologram, Player... players) {
+        send(ImmutableSet.copyOf(players), hologram);
+    }
+
+    /** Send a hologram to all players */
+    default void send(Hologram hologram) {
+        send(Sponge.getServer().getOnlinePlayers(), hologram);
+    }
+
+    /** Send a hologram to the collection of players */
+    default void send(Collection<Player> players, Hologram hologram) {
+        players.forEach(hologram::send);
     }
 }
