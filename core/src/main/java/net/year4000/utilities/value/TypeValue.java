2cd055a7a1fdfa37ef7a9cfed27a3b8a9b3f8083
Joshua Rodriguez <ewized@gmail.com>
2016-09-07 12:04:59 -0700

    Allow suplying with a default value

--- core/src/main/java/net/year4000/utilities/value/TypeValue.java
+++ core/src/main/java/net/year4000/utilities/value/TypeValue.java
@@ -55,8 +55,26 @@ public class TypeValue extends ImmutableValue<Object> {
     }
 
     /** Try to parse the object to an int */
+    public boolean toBoolean(boolean defaultValue) {
+        try {
+            return isBoolean() ? castTo(Boolean.class) : Boolean.valueOf(toString());
+        } catch (ClassCastException error) {
+            return defaultValue;
+        }
+    }
+
+    /** Try to parse the object to an int */
     public int toInt() {
         return isNumber() ? castTo(Number.class).intValue() : Integer.valueOf(toString());
+    }
+
+    /** Try to parse the object to an int */
+    public int toInt(int defaultValue) {
+        try {
+            return isNumber() ? castTo(Number.class).intValue() : Integer.valueOf(toString());
+        } catch (NumberFormatException | ClassCastException error) {
+            return defaultValue;
+        }
     }
 
     /** Try to parse the object to a double */
@@ -64,9 +82,27 @@ public class TypeValue extends ImmutableValue<Object> {
         return isNumber() ? castTo(Number.class).doubleValue() : Double.valueOf(toString());
     }
 
+    /** Try to parse the object to a double */
+    public double toDouble(double defaultValue) {
+        try {
+            return isNumber() ? castTo(Number.class).doubleValue() : Double.valueOf(toString());
+        } catch (NumberFormatException | ClassCastException error) {
+            return defaultValue;
+        }
+    }
+
     /** Try to parse the object to a long */
     public long toLong() {
         return isNumber() ? castTo(Number.class).longValue() : Long.valueOf(toString());
+    }
+
+    /** Try to parse the object to a long */
+    public long toLong(long defaultValue) {
+        try {
+            return isNumber() ? castTo(Number.class).longValue() : Long.valueOf(toString());
+        } catch (ClassCastException | NumberFormatException error) {
+            return defaultValue;
+        }
     }
 
     /** Try to parse the object to a float */
@@ -74,14 +110,41 @@ public class TypeValue extends ImmutableValue<Object> {
         return isNumber() ? castTo(Number.class).floatValue() : Float.valueOf(toString());
     }
 
+    /** Try to parse the object to a float */
+    public float toFloat(float defaultValue) {
+        try {
+            return isNumber() ? castTo(Number.class).floatValue() : Float.valueOf(toString());
+        } catch (NumberFormatException | ClassCastException error) {
+            return defaultValue;
+        }
+    }
+
     /** Try to parse the object to a short */
     public short toShort() {
         return isNumber() ? castTo(Number.class).shortValue() : Short.valueOf(toString());
     }
 
+    /** Try to parse the object to a short */
+    public short toShort(short defaultValue) {
+        try {
+            return isNumber() ? castTo(Number.class).shortValue() : Short.valueOf(toString());
+        } catch (NumberFormatException | ClassCastException error) {
+            return defaultValue;
+        }
+    }
+
     /** Try to parse the object to a byte */
     public byte toByte() {
         return isNumber() ? castTo(Number.class).byteValue() : Byte.valueOf(toString());
+    }
+
+    /** Try to parse the object to a byte */
+    public byte toByte(byte defaultValue) {
+        try {
+            return isNumber() ? castTo(Number.class).byteValue() : Byte.valueOf(toString());
+        } catch (NumberFormatException | ClassCastException error) {
+            return defaultValue;
+        }
     }
 
     /** Try to parse the object to a String */
