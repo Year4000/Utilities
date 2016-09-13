5252840824f83b03581b21704e38ad6901c35d4b
Joshua Rodriguez <ewized@gmail.com>
2016-09-13 15:22:25 -0700

    Properly send 500 error

--- core/src/main/java/net/year4000/utilities/net/router/pipline/ExceptionHandler.java
+++ core/src/main/java/net/year4000/utilities/net/router/pipline/ExceptionHandler.java
@@ -4,9 +4,15 @@
 
 package net.year4000.utilities.net.router.pipline;
 
+import com.google.common.net.HttpHeaders;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
 import net.year4000.utilities.ErrorReporter;
 import net.year4000.utilities.net.router.Router;
 import net.year4000.utilities.net.router.http.Message;
@@ -20,11 +26,19 @@ public class ExceptionHandler extends ChannelDuplexHandler {
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         Router router = ctx.channel().attr(Router.ATTRIBUTE_KEY).get();
         Message message = ctx.channel().attr(Message.ATTRIBUTE_KEY).get();
-        ErrorReporter.builder(cause)
+        String error = ErrorReporter.builder(cause)
             .add("Router: ", router)
             .add("Content Types: ", router.contentTypes(message.endPoint()).toArray())
             .add("Message: ", message)
-            .buildAndReport(System.err);
-        ctx.channel().close();
+            .build().toString();
+        System.err.println(error);
+        message.getRequest().headers().set(HttpHeaders.CONTENT_TYPE, "text/plain");
+        message.setResponse(new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.INTERNAL_SERVER_ERROR));
+        ctx.write(message.makeResponse(Unpooled.copiedBuffer(error.getBytes()))).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
     }
 }
