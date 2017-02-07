60df5e8bd2160364f79a0f67e1eed6dfbe1f3ab6
Joshua Rodriguez <ewized@gmail.com>
2017-02-07 08:40:14 -0800

    Fix utilities debug and add info on how to enable it

--- sponge/src/debug/java/net/year4000/utilities/sponge/UtilitiesDebug.java
+++ sponge/src/debug/java/net/year4000/utilities/sponge/UtilitiesDebug.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Year4000. All Rights Reserved.
+ * Copyright 2017 Year4000. All Rights Reserved.
  */
 
 package net.year4000.utilities.sponge;
@@ -17,6 +17,10 @@ import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.event.game.state.GamePostInitializationEvent;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.event.game.state.GameStartingServerEvent;
+import org.spongepowered.api.plugin.Dependency;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.text.Text;
 
@@ -29,8 +33,10 @@ import javax.imageio.ImageIO;
  * This plugin is used to test various things with Utilities.
  * The class will never be added to the production jar and must be
  * add to the classpath manually to access the testing tools.
+ *
+ * Hint: To enable this look in build.gradle for sponge
  */
-@Plugin(id = "utilities-debug")
+@Plugin(id = "utilities-debug", name = "UtilitiesDebug", dependencies = {@Dependency(id ="utilities")})
 public class UtilitiesDebug {
     private static final List<Hologram> activeHolograms = Lists.newArrayList();
 
